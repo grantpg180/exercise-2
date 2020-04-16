1c4d088d0f800df033d1eb812fd9fbd3313218e0
grantpg180 <62108947+grantpg180@users.noreply.github.com>
2020-04-16 07:57:11 -0400

    exercise 2

---
+++ Exercise2.js
@@ -0,0 +1,9 @@
+var testNum = new Promise(function(resolve,reject){
+	//do something async...and get a value
+	var num < || > 50
+	resolve(num);
+});
+
+testNum.then(function(data){
+	//console.log(data);
+})
